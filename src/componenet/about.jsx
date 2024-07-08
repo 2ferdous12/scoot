28368989e73fb98fb5b07e2125128ed8e4196147
Ferdous020 <sheikhferdous75@gmail.com>
2024-07-08 10:53:38 -0700

    added all file and style

--- src/componenet/about.jsx
+++ src/componenet/about.jsx
@@ -3,43 +3,159 @@
 const about = () => {
     return (
         <div>
-         <div>
-            <div className="h-[200px]">
-                <img src="/public/images/Group 22.png" alt="" />
+            <div>
+                <div className="h-[200px]">
+                    <img className="h-full" src="/public/images/Group 22.png" alt="" />
+                </div>
             </div>
-         </div>
+            <div className="mt-20 flex p-5  flex-col md:flex-row lg:flex-row items-center gap-5  justify-between">
+                <div className="space-y-8">
+                    <h1 className="text-4xl md:text-6xl lg:text-6xl text-[#495567] font-bold">Mobility for <br /> the digital era
+                    </h1>
+                    <p className="w-[350px] md:w-[250px] lg:w-[420px] mt-5 text-xl text-gray-500">Getting around should
+                        be simple (and even fun!) for everyone. We
+                        embrace technology to provide low cost, smart access to scooters at your fingertips.</p>
+                </div>
+                <div>
+                    <img className="w-full" src="/public/images/Bitmap (3).png" alt="" />
+                </div>
+            </div>
 
-         <div className="mt-20 flex p-5  flex-col md:flex-row lg:flex-row items-center  justify-between">
-        <div className="space-y-8">
-            <h1 className="text-4xl md:text-6xl lg:text-6xl text-[#495567] font-bold">Mobility for <br /> the digital era 
-            </h1>
-            <p className="w-[350px] md:w-[250px] lg:w-[420px] mt-5 text-xl text-gray-500">Getting around should 
-                be simple (and even fun!) for everyone. We
-                 embrace technology to provide low cost, smart access to scooters at your fingertips.</p>
-        </div>
 
-        <div>
-            <img src="/public/images/Bitmap (3).png" alt="" />
-        </div>
-    </div>
-    
-    
-    <div className="mt-20 flex p-5 items-center  flex-col md:flex-row lg:flex-row  justify-between">
-         <div className="w-[500px] h-[500px]  rounded-ful">
-            <img className="w-full h-full  rounded-ful" src="/public/images/Bitmap (4).png" alt="" />
-        </div> 
-       
-        <div className="space-y-8">
+            <div className="mt-20 flex p-5 items-center gap-5 flex-col md:flex-row lg:flex-row  justify-between">
+                <div className="w-[370px] md:w-[500px] lg:w-[500px] h-[500px]  rounded-ful">
+                    <img className="w-full h-full  rounded-ful" src="/public/images/Bitmap (4).png" alt="" />
+                </div>
+                <div className="space-y-8">
+                    <h1 className="text-4xl md:text-6xl lg:text-6xl text-[#495567] font-bold">Better urban <br /> living
+                    </h1>
+                    <p className="w-[350px] md:w-[250px] lg:w-[420px] mt-5 text-xl text-gray-500">Scoot is available in 4 major cities
+                        so far. We’re expanding rapidly, so be sure to let us know if you want to see us in your
+                        hometown. We’re aiming to let our scooters loose on 23 cities over the coming year.</p>
+                </div>
+            </div>
 
-            <h1 className="text-4xl md:text-6xl lg:text-6xl text-[#495567] font-bold">Coming to a  <br />
-            city near you</h1>
-            <p className="w-[350px] md:w-[250px] lg:w-[420px] mt-5 text-xl text-gray-500">Scoot is available in 4 major cities 
-                so far. We’re expanding rapidly, so be sure to let us know if you want to see us in your
-                 hometown. We’re aiming to let our scooters loose on 23 cities over the coming year.</p>
-        </div>
 
-    </div>
 
+            <div className="mt-20 mb-[150px] p-6">
+                <h1 className="font-bold text-4xl text-center mt-5 ">Our Value</h1>
+                <div className="mt-20 flex flex-col md:flex-row lg:flex-row justify-between">
+                    <div>
+                        <div className="">
+                            <img className="rounded-full w-[300px] md:w-[200px] lg:w-[300px] h-[300px] md:h-[200px] lg:h-[300px] " src="/public/images/Bitmap (5).png" alt="" />
+                            <div className=" ml-20 md:ml-10 lg:ml-20
+                           bg-[#FCB72B] flex items-center justify-center h-[100px] w-[100px] rounded-full">
+                                <h1 className="text-4xl font-bold">01</h1>
+                            </div>
+                        </div>
+                        <h1 className="text-2xl font-bold text-center mt-2 -ml-20 mb-2">Our tech</h1>
+                        <p className=" md:w-[250px] lg:w-[350px] text-xl font-semibold text-gray-500">We’re using
+                            cutting edge technology to drive accessible urban
+                            transportation
+                            forward. Our fully electric scooters are a joy to ride!</p>
+                    </div>
+
+
+                    <div>
+                        <div className="">
+                            <img className="rounded-full  w-[300px] md:w-[200px] lg:w-[300px] h-[300px] md:h-[200px] lg:h-[300px]  " src="/public/images/Bitmap (6).png" alt="" />
+                            <div className="  ml-20 md:ml-10 lg:ml-20
+                          bg-[#FCB72B] flex items-center justify-center h-[100px] w-[100px] rounded-full">
+                                <h1 className="text-4xl font-bold">02</h1>
+                            </div>
+                        </div>
+                        <h1 className="text-2xl font-bold -ml-20 text-center mt-2 mb-2">Our integrity</h1>
+                        <p className=" md:w-[250px] lg:w-[350px] text-xl font-semibold text-gray-500">We’re using
+                            cutting edge technology to drive accessible urban
+                            transportation
+                            forward. Our fully electric scooters are a joy to ride!</p>
+                    </div>
+
+
+                    <div>
+                        <div className="">
+                            <img className="rounded-full  w-[300px] md:w-[200px] lg:w-[300px] h-[300px] md:h-[200px] lg:h-[300px]  " src="/public/images/Bitmap (7).png" alt="" />
+                            <div className=" ml-20 md:ml-10 lg:ml-20
+                              bg-[#FCB72B] flex items-center justify-center h-[100px] w-[100px] rounded-full">
+                                <h1 className="text-4xl font-bold">03</h1>
+                            </div>
+                        </div>
+                        <h1 className="text-2xl font-bold text-center -ml-20 mt-2 mb-2">Our community</h1>
+                        <p className=" md:w-[250px] lg:w-[350px] text-xl font-semibold text-gray-500">We’re using
+                            cutting edge technology to drive accessible urban
+                            transportation
+                            forward. Our fully electric scooters are a joy to ride!</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mt-20 mb-20">
+                <h1 className="font-bold text-5xl text-center">FAQ</h1>
+
+                <div className="mt-20 flex flex-col gap-5 md:flex-row lg:flex-row">
+                    <div className="w-[100%] md:w-[50%] lg:w-[50%]">
+                        <h1 className="font-bold text-2xl ">How it works</h1>
+                    </div>
+
+                    <div className="w-[100%] md:w-[50%] lg:w-[50%] space-y-5">
+                        <div className="collapse collapse-arrow bg-base-200">
+                            <input type="radio" name="my-accordion-2" defaultChecked />
+                            <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                            <div className="collapse-content">
+                                <p>hello</p>
+                            </div>
+                        </div>
+                        <div className="collapse collapse-arrow bg-base-200">
+                            <input type="radio" name="my-accordion-2" />
+                            <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                            <div className="collapse-content">
+                                <p>hello</p>
+                            </div>
+                        </div>
+                        <div className="collapse collapse-arrow bg-base-200">
+                            <input type="radio" name="my-accordion-2" />
+                            <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                            <div className="collapse-content">
+                                <p>hello</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="mt-20 flex flex-col gap-5 md:flex-row lg:flex-row">
+                    <div className="w-[100%] md:w-[50%] lg:w-[50%]">
+                        <h1 className="font-bold text-2xl ">How it works</h1>
+                    </div>
+
+                    <div className="w-[100%] md:w-[50%] lg:w-[50%] space-y-5">
+                        <div className="collapse collapse-arrow bg-base-200">
+                            <input type="radio" name="my-accordion-2" defaultChecked />
+                            <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                            <div className="collapse-content">
+                                <p>hello</p>
+                            </div>
+                        </div>
+                        <div className="collapse collapse-arrow bg-base-200">
+                            <input type="radio" name="my-accordion-2" />
+                            <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                            <div className="collapse-content">
+                                <p>hello</p>
+                            </div>
+                        </div>
+                        <div className="collapse collapse-arrow bg-base-200">
+                            <input type="radio" name="my-accordion-2" />
+                            <div className="collapse-title text-xl font-medium">Click to open this one and close others</div>
+                            <div className="collapse-content">
+                                <p>hello</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
+            </div>
 
         </div>
     );
